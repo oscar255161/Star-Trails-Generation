afcb4bb5e91c4250c7f40fffaacdeac0540d0216
oscar255161 <156433303+oscar255161@users.noreply.github.com>
2024-11-27 21:11:00 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -164,14 +164,14 @@ def find_brightest_star(img, x_line_img, star_threshold, max_contours=150):
     with Pool() as pool:
         results = pool.map(process_contour, args)
 
-    # 过滤None结果并解包
+    
     processed_contours = [res for res in results if res is not None]
 
-    # 如果轮廓太多，只保留最亮的N个
+    
     if len(processed_contours) > max_contours:
         processed_contours = sorted(processed_contours, key=lambda x: x[0], reverse=True)[:max_contours]
 
-    # 提取星团信息
+   
     star_clusters = [{'brightness': brightness, 'location': location, 'area': area} 
                      for brightness, location, area in processed_contours]
 
@@ -186,7 +186,7 @@ def find_brightest_star(img, x_line_img, star_threshold, max_contours=150):
 # ================= ==other================================
 def del_sky_v3(img):
 
-    # 连通组件分析
+   
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
 
     # init_setting
@@ -196,7 +196,7 @@ def del_sky_v3(img):
     total_area = img.shape[0] * img.shape[1]
     min_area_threshold = total_area / 500
 
-    # 用于存储大组件中每个x的最小y的字典
+    
     x_min_y = {}
     a = 0
 
@@ -206,7 +206,7 @@ def del_sky_v3(img):
         component_height = stats[label, cv2.CC_STAT_HEIGHT]
         component_top = stats[label, cv2.CC_STAT_TOP]
 
-        # 如果组件顶部小于图片1/7高度或者组件宽度大于图片宽度的一半，则跳过
+        
         if component_top < one_seventh_height and component_width < half_width:
             continue
         len_limit=30
@@ -219,14 +219,14 @@ def del_sky_v3(img):
                     if x not in x_min_y or y < x_min_y[x]:
                         x_min_y[x] = y
 
-    # 如果没有找到大的组件，则创建一张全白图像
+    
     if a == 0:
         img = np.ones_like(img, dtype=np.uint8) * 255
     else:
-        # 创建一张全黑的图像
+       
         img = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
         for x, y in x_min_y.items():
-            img[y, x] = 255  # 在BGR格式下为红色
+            img[y, x] = 255  
 
     return img
 
@@ -249,7 +249,7 @@ def connect_line_v4(img):
         leftmost_x = np.min(white_x)
         rightmost_x = np.max(white_x)
     
-        # 获取对应的y值
+        
         y_for_leftmost_x = white_y[np.argmin(white_x)]
         y_for_rightmost_x = white_y[np.argmax(white_x)]
     
@@ -285,21 +285,21 @@ class Application:
         self.output_path = output_path
         self.root.title("星軌圖")
     
-    # 使用 Frame 来容纳按钮，这样可以更容易地控制布局
+    
         button_frame = tk.Frame(root)
-        button_frame.pack(side=tk.TOP, pady=10)  # 将按钮的框架放在顶部，并添加一些垂直间距
+        button_frame.pack(side=tk.TOP, pady=10)  
 
-    # 创建按钮并将它们放入按钮框架中
+    
         self.load_button = tk.Button(button_frame, text="Load Image", command=self.load_image)
-        self.load_button.pack(side=tk.LEFT, padx=5)  # 放在左边，并添加一些水平间距
+        self.load_button.pack(side=tk.LEFT, padx=5) 
 
         self.auto_button = tk.Button(button_frame, text="Auto Find Star", command=self.auto_find_star)
-        self.auto_button.pack(side=tk.LEFT, padx=5)  # 放在左边的下一个位置
-
+        self.auto_button.pack(side=tk.LEFT, padx=5)  
+        
         self.manual_button = tk.Button(button_frame, text="Manual Select Star", command=self.manual_select_star)
-        self.manual_button.pack(side=tk.LEFT, padx=5)  # 同上
+        self.manual_button.pack(side=tk.LEFT, padx=5)  
 
-    # 创建画布并填充整个窗口剩余空间
+   
         self.canvas = tk.Canvas(root)
         self.canvas.pack(fill="both", expand=True)
 
@@ -307,47 +307,47 @@ class Application:
  
     def load_image(self):
         file_path = filedialog.askopenfilename()
-        if file_path:  # 检查是否选择了文件
+        if file_path:
             original_img = cv2.imread(file_path)
             #original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
-            self.original_img = original_img  # 保存原始图像用于后续处理
-            self.image_name = os.path.basename(file_path)  # 保存图像名称
+            self.original_img = original_img  
+            self.image_name = os.path.basename(file_path) 
         
-        # 对图像进行缩放以适应画布
+       
             scaled_img = self.scale_image_for_display(original_img)
             scaled_img = cv2.cvtColor(scaled_img, cv2.COLOR_BGR2RGB)
-        # 显示缩放后的图像
+        
             self.photo = ImageTk.PhotoImage(image=Image.fromarray(scaled_img))
             
             self.display_image(scaled_img)
 
     def scale_image_for_display(self, img):
-    # 获取画布大小
-        self.canvas.update()  # 确保我们有最新的画布大小
+    
+        self.canvas.update()  
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
 
-    # 计算缩放比例
+    
         img_height, img_width = img.shape[:2]
         scale_width = canvas_width / img_width
         scale_height = canvas_height / img_height
         self.scale = min(scale_width, scale_height)
 
-    # 缩放图像
+ 
         scaled_width = int(img_width * self.scale)
         scaled_height = int(img_height * self.scale)
         return cv2.resize(img, (scaled_width, scaled_height))
 
     def display_image(self, scaled_img):
-    # 计算图像居中时的位置
+
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
         img_width, img_height = scaled_img.shape[1], scaled_img.shape[0]
         x_position = (canvas_width - img_width) // 2
         y_position = (canvas_height - img_height) // 2
 
-        # 清除画布并在画布上居中显示图像
-        self.canvas.delete("all")  # 清除画布上的所有元素
+     
+        self.canvas.delete("all") 
         self.canvas.create_image(x_position, y_position, image=self.photo, anchor=tk.NW)
 
     def auto_find_star(self):
@@ -396,22 +396,22 @@ class Application:
                 combine_frame=120
             star_clusters, (brightest_x, brightest_y), _, _ = find_brightest_star(self.original_img, x_line_img, star_threshold)
             self.create_star_trail_gif((brightest_x, brightest_y), x_line_img, star_clusters)
-            self.root.destroy()  # 处理完成后关闭窗口
+            self.root.destroy() 
     def manual_select_star(self):
         self.canvas.bind("<ButtonPress-1>", self.on_click)
 
     def on_click(self, event):
-    # 获取点击点在缩放图像上的坐标
+  
         canvas_x, canvas_y = event.x, event.y
 
-    # 计算图像在画布上的偏移
+ 
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
         img_width, img_height = self.photo.width(), self.photo.height()
         x_offset = (canvas_width - img_width) // 2
         y_offset = (canvas_height - img_height) // 2
  
-    # 转换点击坐标以获取原始图像上的坐标
+ 
         if hasattr(self, 'scale') and self.scale != 0:
             original_x = int((canvas_x - x_offset) / self.scale)
             original_y = int((canvas_y - y_offset) / self.scale)
@@ -419,9 +419,8 @@ class Application:
                 self.selected_point = (original_x, original_y)
                 print(f"Selected point in original image: {self.selected_point}")
 
-            # 手动模式下的图像处理逻辑
                 if self.original_img is not None:
-                # 与自动模式相同的后续处理
+         
                     processed_img = sobel_filter(self.original_img)
                     processed_img = blur_Canny(processed_img)
                     processed_img = close_image(processed_img)
@@ -466,10 +465,10 @@ class Application:
                         combine_frame=120
                     star_clusters, _, _, _ = find_brightest_star(self.original_img, x_line_img, star_threshold)
 
-                # 使用用户选择的星星坐标创建星轨
+        
                     self.create_star_trail_gif((original_x, original_y), x_line_img, star_clusters)
-                # 如果需要在点击后关闭窗口，请取消注释下面的代码
-                    self.root.destroy()  # 处理完成后关闭窗口
+          
+                    self.root.destroy() 
             else:
                 print("Clicked outside the image area.")
         else:
@@ -478,7 +477,7 @@ class Application:
 
 
     def create_star_trail_gif(self, center, x_line, star_clusters):
-    # 确保 output_path 和 image_name 可用
+   
         if hasattr(self, 'original_img') and self.original_img is not None and hasattr(self, 'image_name'):
                 height, width = self.original_img.shape[:2]
                 if width * height < 1000000:
